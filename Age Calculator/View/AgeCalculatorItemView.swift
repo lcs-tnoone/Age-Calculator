b90be2b4d56b1a5f570513c93bad10522512fe15
Thomas Noone <tnoone@lcs.on.ca>
2025-03-03 16:14:23 -0500

    made save button and history saving work

---
+++ Age Calculator/View/AgeCalculatorItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AgeCalculatorItemView.swift
+//  Age Calculator
+//
+//  Created by Thomas Noone on 2025-03-03.
+//
+
+import SwiftUI
+
+struct AgeCalculatorItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgeCalculatorItemView()
+}
