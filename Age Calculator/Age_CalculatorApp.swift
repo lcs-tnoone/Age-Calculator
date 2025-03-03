05dafb8ca9a5949b3f677e43e29cb043093bcaa5
Thomas Noone <tnoone@lcs.on.ca>
2025-03-03 14:51:37 -0500

    Initial Commit

---
+++ Age Calculator/Age_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Age_CalculatorApp.swift
+//  Age Calculator
+//
+//  Created by Thomas Noone on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct Age_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
