b81fd36f96a277f2c21929f1a18ab7db28ff7365
Thomas Noone <tnoone@lcs.on.ca>
2025-03-03 15:28:38 -0500

    added basic model and viewmodel

---
+++ Age Calculator/View/AgeCalculatorView.swift
@@ -0,0 +1,18 @@
+//
+//  AgeCalculatorView.swift
+//  Age Calculator
+//
+//  Created by Thomas Noone on 2025-03-03.
+//
+
+import SwiftUI
+
+struct AgeCalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgeCalculatorView()
+}
